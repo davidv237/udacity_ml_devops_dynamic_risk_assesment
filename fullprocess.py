17842d851017d50aad8c5db7084c2eda31af52b7
David Venzke <digerian@protonmail.com>
2023-05-07 09:36:08 +0200

    automation of full process, check_and_read_new_data function

--- fullprocess.py
+++ fullprocess.py
@@ -5,6 +5,10 @@ import scoring
 import deployment
 import diagnostics
 import reporting
+import ingestion
+
+import json
+import os
 
 ##################Check and read new data
 #first, read ingestedfiles.txt
@@ -23,25 +27,51 @@ def check_and_read_new_data():
     with open(ingested_files_path, 'r') as f:
         ingested_files = f.read().splitlines()
 
+    print(ingested_files)
+
     # Check for new files in the input_folder_path
     input_folder_path = config['input_folder_path']
     all_files = os.listdir(input_folder_path)
 
-    new_files = [file for file in all_files if file not in ingested_files]
+    new_files = [file for file in all_files if file not in ingested_files and file.endswith('.csv')]
 
-    # Ingest new data if there are any new files
-    if new_files:
-        ingestion(config)
+    if not new_files:
+        print("No new files found. Ending the process.")
+        return
+
+    # If new data is found, proceed to the next step (checking for model drift)
+    print(f"New files found: {', '.join(new_files)}")
 
     return new_files
 
 
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
-
-
 ##################Checking for model drift
 #check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+
+def detect_model_drift():
+    # Check and read new data
+    new_files = check_and_read_new_data()
+
+    # Deciding whether to proceed (first time)
+    if not new_files:
+        print("No new files found. Ending the process.")
+        return
+
+    # If new data is found, proceed to check for model drift
+    print(f"New files found: {', '.join(new_files)}")
+
+    # Load configuration
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    model_drift, new_score = check_for_model_drift(new_files, config)
+
+    if model_drift:
+        print(f"Model drift detected. New score: {new_score:.4f}")
+        # Proceed with the rest of the deployment process
+    else:
+        print(f"No model drift detected. New score: {new_score:.4f}")
+        # No need to proceed with the rest of the deployment process
 
 
 ##################Deciding whether to proceed, part 2
